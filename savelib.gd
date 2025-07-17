a0977ddb671963750a63b378eb1654d08735b3a2
CrazyCat911 <127078477+CrazyCat911@users.noreply.github.com>
2025-07-17 20:15:55 +1000

    Add doc comments to the savelib

--- savelib.gd
+++ savelib.gd
@@ -2,11 +2,11 @@ extends Node
 
 var savegame_path = "user://savegame.save"
 
-func save(data) -> void:
+func save(data) -> void: ## Saves [param data]. To load, use [method SaveLib.load]
 	var file = FileAccess.open(savegame_path, FileAccess.WRITE)
 	file.store_var(data, true)
 
-func load() -> Variant:
+func load() -> Variant: ## Loads data from the location made by [method SaveLib.save]
 	if FileAccess.file_exists(savegame_path):
 		var file = FileAccess.open(savegame_path, FileAccess.READ)
 		
