0d14861710ddc6353838a3793e75b366afd3a58f
Crazy_Cat911 <127078477+CrazyCat911@users.noreply.github.com>
2025-07-19 13:31:47 +1000

    Create a polygon utils for easier manipulation of polygon shapes

---
+++ polygon_utils.gd
@@ -0,0 +1,25 @@
+extends Node
+
+## Returns [code]true[/code] if [param poly_a_node] is fully inside [param poly_b_node]
+func is_polygon_fully_contained(poly_a_node: Polygon2D, poly_b_node: Polygon2D) -> bool:
+	var poly_a: PackedVector2Array = get_global_polygon(poly_a_node)
+	var poly_b: PackedVector2Array = get_global_polygon(poly_b_node)
+
+	# Ensure both polygons are counter-clockwise
+	if Geometry2D.is_polygon_clockwise(poly_a):
+		poly_a.reverse()
+
+	if Geometry2D.is_polygon_clockwise(poly_b):
+		poly_b.reverse()
+
+	# If clipping returns an empty result, poly_b is fully inside poly_a
+	var result = Geometry2D.clip_polygons(poly_a, poly_b)
+	return result.is_empty()
+
+# Converts a Polygon2D node to global space polygon
+func get_global_polygon(polygon_node: Polygon2D) -> PackedVector2Array:
+	var global_polygon = PackedVector2Array()
+	var global_xform = polygon_node.get_global_transform()
+	for local_point in polygon_node.polygon:
+		global_polygon.append(global_xform * local_point)
+	return global_polygon
