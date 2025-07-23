c3c7d8cc116397730ac5bcbcb95d8c477f9b0e1f
CrazyCat911 <127078477+CrazyCat911@users.noreply.github.com>
2025-07-23 21:00:36 +1000

    Make the texture scale correctly

--- drag_and_drop.gd
+++ drag_and_drop.gd
@@ -1,4 +1,4 @@
-extends Sprite2D
+extends Node2D
 
 func _process(delta: float) -> void:
 	global_position = lerp(global_position, get_global_mouse_position(), 25 * delta)
--- level_stub.gd
+++ level_stub.gd
@@ -11,7 +11,7 @@ var config: Dictionary = {
 }
 
 func _ready() -> void:
-	$Shop.init_shop({"solar_panel": 1, "mice": 42}, {})
+	$Shop.init_shop({"solar_panel": 1}, {})
 
 func _on_shop_shop_ready() -> void:
 	print("Shop is ready!")
@@ -19,19 +19,28 @@ func _on_shop_shop_ready() -> void:
 func _on_shop_item_selected(item_name: String) -> void:
 	print("Item Selected! %s" % item_name)
 	selected_item = item_name
+
+	var item_config: Dictionary = config.get(item_name, {})
 	
-	var item_sprite := Sprite2D.new()
+	var item_node := Node2D.new()
 	var item_collision := Area2D.new()
 	var item_collision_shape := CollisionShape2D.new()
 	var item_shape := RectangleShape2D.new()
-	var item_size: int = config.get(item_name, {}).get("size", 0)
+	var item_size: int = item_config.get("size", 0)
+	var item_sprite: Sprite2D = Sprite2D.new()
+	var item_texture: Texture2D = item_config.get("texture")
+	var texture_size: Vector2 = item_texture.get_size()
 	item_shape.size = Vector2(item_size, item_size)
 	item_collision_shape.shape = item_shape
+
+	item_sprite.texture = item_config.get("texture")
+
+	item_sprite.scale = item_shape.size / texture_size
+	
 	item_collision.add_child(item_collision_shape)
-	item_sprite.add_child(item_collision)
+	item_node.add_child(item_sprite)
+	item_node.add_child(item_collision)
 	
-	item_sprite.texture = config.get(item_name, {}).get("texture", load("res://assets/solar_panel.png"))
+	item_node.set_script(drag_and_drop_script)
 	
-	item_sprite.set_script(drag_and_drop_script)
-	
-	self.add_child(item_sprite)
+	self.add_child(item_node)
