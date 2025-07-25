4c966120d7b6d099181d301a1ff49122aeca80da
Crazy_Cat911 <127078477+CrazyCat911@users.noreply.github.com>
2025-07-25 16:04:00 +1000

    Track and remove previously selected node
    Introduces a selected_node variable to keep reference to the currently selected item node. When a new item is selected, the previous node is freed before adding the new one, ensuring only one selected node exists at a time.

--- level_stub.gd
+++ level_stub.gd
@@ -1,6 +1,7 @@
 extends Node2D
 
 var selected_item: String
+var selected_node: Node2D
 var drag_and_drop_script = load("res://drag_and_drop.gd")
 
 var config: Dictionary = {
@@ -19,6 +20,9 @@ func _on_shop_shop_ready() -> void:
 func _on_shop_item_selected(item_name: String) -> void:
 	print("Item Selected! %s" % item_name)
 	selected_item = item_name
+	
+	if selected_node:
+		selected_node.queue_free()
 
 	var item_config: Dictionary = config.get(item_name, {})
 	
@@ -44,3 +48,4 @@ func _on_shop_item_selected(item_name: String) -> void:
 	item_node.set_script(drag_and_drop_script)
 	
 	self.add_child(item_node)
+	selected_node = item_node
