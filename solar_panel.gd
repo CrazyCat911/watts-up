549885cb293154c77ef5b4862caf71dbff81eddc
CrazyCat911 <127078477+CrazyCat911@users.noreply.github.com>
2025-07-30 20:47:05 +1000

    Remove useless and buggly lerp

--- solar_panel.gd
+++ solar_panel.gd
@@ -2,8 +2,8 @@ extends Node2D
 
 signal placed() ## Signals when the panel is placed
 
-func _process(delta: float) -> void:
-	global_position = lerp(global_position, get_global_mouse_position(), 25 * delta)
+func _process(_delta: float) -> void:
+	global_position = get_global_mouse_position()
 	
 	if Input.is_action_just_pressed("click"):
 		placed.emit()
