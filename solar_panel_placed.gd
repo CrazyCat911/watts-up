8fbcf9ca483ad01b9e02bba480000ab8732c1204
Crazy_Cat911 <127078477+CrazyCat911@users.noreply.github.com>
2025-08-18 13:27:42 +1000

    FIX BUG

--- solar_panel_placed.gd
+++ solar_panel_placed.gd
@@ -3,14 +3,11 @@ extends Node2D
 @export var item_type: String
 @export var poly: PackedVector2Array
 
-var mouse_inside: bool = false
 signal delete(node: Node2D)
 
 func _init() -> void:
 	var area_node: Area2D = $Area2D
 	area_node.input_pickable = true
-	area_node.mouse_entered.connect(func(): mouse_inside = true)
-	area_node.mouse_exited.connect(func(): mouse_inside = false)
 
 	poly = PolygonUtils.shape_to_polygon($Area2D/CollisionPolygon2D)
 	
@@ -18,5 +15,5 @@ func _init() -> void:
 
 func _process(_delta: float) -> void:
 	if Input.is_action_just_pressed("click"):
-		if mouse_inside:
+		if Geometry2D.is_point_in_polygon(get_global_mouse_position(), poly):
 			delete.emit(self)
